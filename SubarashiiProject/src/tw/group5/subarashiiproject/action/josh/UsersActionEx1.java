c7ad7e1356067ca65c56c03304e95cc273555ff7
joshysunnysun <ji1997819@gmail.com>
2021-05-23 00:52:20 +0800

    josh#update switch case還沒ok
    待換成if判斷

--- SubarashiiProject/src/tw/group5/subarashiiproject/action/josh/UsersActionEx1.java
+++ SubarashiiProject/src/tw/group5/subarashiiproject/action/josh/UsersActionEx1.java
@@ -11,9 +11,10 @@ import tw.group5.subarashiiproject.util.HibernateUtil;
 public class UsersActionEx1 {
 
 	public static void main(String[] args) {
-		
 		UsersActionEx1 action = new UsersActionEx1();
-		action.processAction();
+		boolean loginResult = action.processAction();
+		action.switchCase();
+
 	}
 
 	public boolean processAction() {
@@ -48,6 +49,73 @@ public class UsersActionEx1 {
 		return validity;
 	}
 	
+	int showCount[] = new int[42]; //出現次數
+	int num[] = new int[42];
+	
+	public void count() {
+		for (int i=1 ; i<100000 ; i++) {
+			int randomNumer = (int)(Math.random()*42 +1); //randomNum接收亂數,把float轉成int
+			showCount[randomNumer-1]++;
+		}
+	}
+	
+	public void sortarray() { //排序出現次數(大到小)
+		
+		for(int i=0 ; i<num.length ; i++) {
+			num[i]=i+1;
+		}
+		while (true) {
+			int count = 0;
+			for(int i=0 ; i<showCount.length-1 ; i++) {
+				int countOrder = 0, numOrder = 0;
+				if(showCount[i] < showCount[i+1]) {
+					countOrder = showCount[i];
+					showCount[i] = showCount[i+1];
+					showCount[i+1] = countOrder;
+					
+					numOrder = num[i];
+					num[i] = num[i+1];
+					num[i+1] = numOrder;
+					count++;
+				}
+			}
+			if(count == 0) {
+				break;
+			}
+		}
+		
+		for(int i=0 ; i<=6 ; i++) {
+			System.out.println(num[i] + ":\t" + showCount[i] + "次");
+		}
+	}
+	
+	public void switchCase() {
+		Scanner sc = new Scanner(System.in);
+		String input1 = sc.nextLine();
+		switch (input1) {
+		case "lottery":
+			System.out.println("Lottery");
+			UsersActionEx1 action1 = new UsersActionEx1();
+			action1.count();
+			action1.sortarray();
+			break;
+		case "test":
+			System.out.println("test!");
+			break;
+		
+		default:
+			break;
+		}
+		System.out.println("程式結束");
+	}
 	
 
 }
+
+
+
+
+
+
+
+
